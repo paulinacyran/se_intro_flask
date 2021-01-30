087477c92210c60bd2502516fa12cb3ee3c68c5e
Student <student@TesterVM-77.edu-wsb.local>
2021-01-30 11:47:10 +0100

    dodanie funkcji podziel

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 moje_imie = 'Paulina'
-noje_zwierze = 'Kot'
+moje_zwierze = 'Kot'
 
 @app.route('/')
 def hello_world():
@@ -12,6 +13,26 @@ def hello_world():
 def kto():
     return moje_zwierze
 
+# http://127.0.0.1:5000/oblicz?l1=10&l2=20&op=plus
+@app.route('/oblicz')
+def oblicz():
+    l1 = int(request.args.get('l1'))
+    l2 = int(request.args.get('l2'))
+    op = request.args.get('op')
+    if op == "plus":
+        s = l1 + l2
+        return f'{s}'
+    return ""
+
+@app.route('/podziel')
+def podziel():
+    l1 = int(request.args.get('l1'))
+    l2 = int(request.args.get('l2'))
+    op = request.args.get('op')
+    if op == "ukosnik":
+        s = l2 / l1
+        return f'{s}'
+    return ""
 
 @app.route('/wiadomosc')
 def wiadomosc():
