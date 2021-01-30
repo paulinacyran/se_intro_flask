0989e245dc5bda589afec8824226cf78a0b91b12
paulinacyran <paulinacyran@users.noreply.github.com>
2021-01-30 12:10:21 +0100

    dodanie funkcji dzielenia2

--- hello.py
+++ hello.py
@@ -16,23 +16,50 @@ def kto():
 # http://127.0.0.1:5000/oblicz?l1=10&l2=20&op=plus
 @app.route('/oblicz')
 def oblicz():
-    l1 = int(request.args.get('l1'))
-    l2 = int(request.args.get('l2'))
+    l1_arg = request.args.get('l1')
+    if l1_arg is None:
+        return 'brak argumentu l1'
+    l1 = int(l1_arg)
+
+    l2_arg = request.args.get('l2')
+    l2 = int(l2_arg)
     op = request.args.get('op')
-    if op == "plus":
+    if op == 'plus':
         s = l1 + l2
         return f'{s}'
     return ""
 
+    # l1 = int(request.args.get('l1'))
+    # l2 = int(request.args.get('l2'))
+    # op = request.args.get('op')
+    # if op == "plus":
+    #     s = l1 + l2
+    #     return f'{s}'
+    # return ""
+
+# http://127.0.0.1:5000/podziel?l1=10&l2=20&op=ukosnik
 @app.route('/podziel')
 def podziel():
-    l1 = int(request.args.get('l1'))
-    l2 = int(request.args.get('l2'))
+    l1_arg = request.args.get('l1')
+    if l1_arg is None:
+        return 'brak argumentu l1'
+    l1 = int(l1_arg)
+
+    l2_arg = request.args.get('l2')
+    l2 = int(l2_arg)
     op = request.args.get('op')
-    if op == "ukosnik":
-        s = l2 / l1
+    if op == 'ukosnik':
+        s = l1 / l2
         return f'{s}'
     return ""
+
+    # l1 = int(request.args.get('l1'))
+    # l2 = int(request.args.get('l2'))
+    # op = request.args.get('op')
+    # if op == "ukosnik":
+    #     s = l2 / l1
+    #     return f'{s}'
+    # return ""
 
 @app.route('/wiadomosc')
 def wiadomosc():
